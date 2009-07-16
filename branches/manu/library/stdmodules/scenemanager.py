594aa49f97b434f04b11b47ca6ea4ab9336306e1
weap88 <weap88@02bf6da4-a4df-11dd-bc7a-dd6b2ed99ab5>
2009-07-16 13:20:49 +0000

    cambios en el scenemanager.py, lanza las excepciones correctas y anyadido cargarycambiar

--- branches/manu/library/stdmodules/scenemanager.py
+++ branches/manu/library/stdmodules/scenemanager.py
@@ -8,25 +8,30 @@ from library.general import xmlconfig
 
 class SceneManager (module.Module):
 
-    def __init__(self, loader):
+    def __init__(self, factory):
         module.Module.__init__(self)
         self.scenes=sdwak.SDWAK()
-        self.scene=sceneapp.SceneApp()  
-        self.loader=loader
-        
-    def change_scene(self,name_scene):
-        self.scene.end_scene()
         self.scene=sceneapp.SceneApp()
-        xmlconfig.cargar_estado(self.scenes[name_scene],self.scene.add,self.loader)
-        self.scene.start_scene()
+        self.factory=factory
         
-    def charge_scenes(self,path):
+    def charge_scene(self, name_scene, xmlfilename):
+        self.scenes[name_scene]=xmlfilename
+        
+    def charge_scenes(self, path):
+        raise NotImplementedError()
         pass
         
-    def charge_scene(self,name_scene ,xmlfilename):
-        self.scenes[name_scene]=xmlfilename
-
-    def new_event(self,event):
+    def change_scene(self, name_scene):
+        self.scene.end_scene()
+        self.scene=sceneapp.SceneApp()
+        xmlconfig.cargar_estado(self.scenes[name_scene],self.scene.add,self.factory)
+        self.scene.start_scene()
+                
+    def charge_and_change_scene(self,name_scene, xmlfilename):
+        self.charge_scene(name_scene, xmlfilename)
+        self.change_scene(name_scene)
+        
+    def new_event(self, event):
         return self.scene.new_event(event)
         
     def update(self):
